1e5712d84d4a6953d8caa12bed8d9170a31eb889
Aaron <59898602+AaronCrum@users.noreply.github.com>
2021-03-08 14:29:55 -0800

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,11 @@
+public class ASCII {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
